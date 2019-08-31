687f1ccbe8ecfa3b10011c0fd459416b469905dd
Alejandro Chanquín <chanquin921@gmail.com>
2019-08-30 20:38:18 -0600

    Nueva Plantilla HTML

--- Views/Conexion.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-$host = '127.0.0.1';
-$user = 'root';
-$password = '';
-$database = 'esperanza';
-
-$mysqli = mysqli_connect($host, $user, $password, $database) or die(mysqli_connect_error());
--- Views/cerrarSesion.php
+++
@@ -1,8 +0,0 @@
-<?php
-session_start();
-session_destroy();
-$server = $_SERVER['HTTP_HOST'];
-?>
-<script type='text/javascript'>
-        window.location='<?php echo "http://$server/login/"; ?>'
-        </script>
